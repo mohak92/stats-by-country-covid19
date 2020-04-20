13a2f8fade24e2c00b5217722ada1c627b8af584
mohak92 <tamhane.mohak@gmail.com>
2020-04-19 23:13:11 -0400

    component for cards

---
+++ src/components/inffectedCountry/InffectedCountryCard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Card, CardHeader, Avatar, CardContent, Grid } from '@material-ui/core';
+
+export default function InffectedCountryCard(){
+    return (
+        <Card></Card>
+    );
+}
