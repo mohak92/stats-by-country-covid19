689a39d7f101f6e4ccc79e89906470f9ff161b97
mohak92 <tamhane.mohak@gmail.com>
2020-04-20 16:37:15 -0400

    create n number of cards for infected countries

--- src/components/inffectedCountry/InffectedCountryList.js
+++ src/components/inffectedCountry/InffectedCountryList.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { Container, Grid } from '@material-ui/core';
+import React from "react";
+import { Container, Grid } from "@material-ui/core";
 
-import InffectedCountryCard from './InffectedCountryCard';
+import InffectedCountryCard from "./InffectedCountryCard";
 
-export default function InffectedCountryList() {
-    return (
-        <Container>
-            <Grid>
-                
-            </Grid>
-        </Container>
-    );
+export default function InffectedCountryList({
+  inffectedCountries,
+  updatedAt,
+}) {
+  return (
+    <Container>
+      <h1 style={{ marginBottom: "0" }}>Infected countries</h1>
+      <p style={{ color: "#0e5077", marginTop: "0" }}>updated at {updatedAt}</p>
+      <Grid spacing={1} container>
+        {inffectedCountries.map((country) => (
+          <Grid key={country.country_name} lg={6} xs={12} sm={12} item>
+            <InffectedCountryCard {...country} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
