4e0025a472d1913e9bf7878d928f10ddb2255193
mohak92 <tamhane.mohak@gmail.com>
2020-04-20 14:55:45 -0400

    api call  to fetch data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,57 @@ import InffectedCountryList from './components/inffectedCountry/InffectedCountry
 import InffectedCountryMap from './components/inffectedCountry/InffectedCountryMap';
 
 function App() {
+
+  let [inffectedCountries, setinffectedCountries] = useState([]);
+  let [updatedAt, setUpdatedAt] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let inffectedCountriesResponse = await fetch(
+        'https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php',
+        {
+          headers: {
+            'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
+            'x-rapidapi-key': '',
+          },
+        },
+      );
+
+      let allCountriesResponse = await fetch('https://restcountries.eu/rest/v2/all');
+
+      let allCountries = [];
+      if (allCountriesResponse.ok) {
+        allCountries = await allCountriesResponse.json();
+      }
+
+      if (inffectedCountriesResponse.ok) {
+        let { countries_stat, statistic_taken_at } = await inffectedCountriesResponse.json();
+        setinffectedCountries(
+          countries_stat.map(stat => {
+            let countryFound = allCountries.find(
+              country =>
+                //country.name.toUpperCase().indexOf(stat.country_name.toUpperCase()) !== -1 ||
+                country.name.toUpperCase() === stat.country_name.toUpperCase() ||
+                country.altSpellings.findIndex(
+                  alt => alt.toUpperCase() === stat.country_name.toUpperCase(),
+                ) !== -1,
+            );
+            if (!countryFound) countryFound = {};
+            return {
+              ...stat,
+              urlFlag: countryFound.flag,
+              latlng: countryFound.latlng,
+            };
+          }),
+        );
+
+        setUpdatedAt(statistic_taken_at);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Grid container>
       <Grid style={{ height: '100vh', overflowY: 'auto' }} lg={6} sm={12} item>
