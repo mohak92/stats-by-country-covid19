3154b9057ec584bb45f2e45ebbafc45152aa841f
mohak92 <tamhane.mohak@gmail.com>
2020-04-19 23:12:15 -0400

    component to render list of countries

---
+++ src/components/inffectedCountry/InffectedCountryList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Grid } from '@material-ui/core';
+
+import InffectedCountryCard from './InffectedCountryCard';
+
+export default function InffectedCountryList() {
+    return (
+        <Container>
+            <Grid>
+                
+            </Grid>
+        </Container>
+    );
+}
