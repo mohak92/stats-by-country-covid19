274b2948a3d8113b7fb2e7a83de2e8d06e369937
mohak92 <tamhane.mohak@gmail.com>
2020-04-19 23:13:45 -0400

    layout updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Grid } from '@material-ui/core';
 
+import InffectedCountryList from './components/inffectedCountry/InffectedCountryList';
 import InffectedCountryMap from './components/inffectedCountry/InffectedCountryMap';
 
 function App() {
   return (
     <Grid container>
+      <Grid style={{ height: '100vh', overflowY: 'auto' }} lg={6} sm={12} item>
+        <InffectedCountryList/>
+      </Grid>
       <Grid style={{ height: '100vh' }} lg={6} sm={12} item>
         <InffectedCountryMap/>
       </Grid>
