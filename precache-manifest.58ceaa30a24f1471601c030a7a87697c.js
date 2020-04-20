ed7cf3ac1498e478ee9cc36c3620229ef4e35ef5
mohak92 <tamhane.mohak@gmail.com>
2020-04-19 22:56:50 -0400

    Updates

---
+++ precache-manifest.58ceaa30a24f1471601c030a7a87697c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b5cab8118121e3f72db1daff397895",
+    "url": "/covid19-stats/index.html"
+  },
+  {
+    "revision": "beb6a90fe5618ed467d6",
+    "url": "/covid19-stats/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc5e7079b4829caa60d1",
+    "url": "/covid19-stats/static/js/2.e603998b.chunk.js"
+  },
+  {
+    "revision": "3a08a7c8f8e6231522abe29a929e6912",
+    "url": "/covid19-stats/static/js/2.e603998b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb6a90fe5618ed467d6",
+    "url": "/covid19-stats/static/js/main.d572d7b1.chunk.js"
+  },
+  {
+    "revision": "b154efa24bde54d4a81c",
+    "url": "/covid19-stats/static/js/runtime-main.37db655e.js"
+  }
+]);
